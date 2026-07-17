7914ceb809590254578a5e219d198c2da385cb41
Marcelo Pessoa <josemarcelopessoadasilva@gmail.com>
2026-07-17 19:10:39 -0300

    adicionar estrtura de abstracao para criacao de servidores

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,26 @@
-import http from 'node:http';
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/customers", (request, response) => {
+  const customers = [
+    {
+      name: "Marcelo Pessoa",
+      status: true,
+    },
+    {
+      name: "Ralf",
+      status: true,
+    },
+    {
+      name: "Programação",
+      status: false,
+    },
+  ];
+
+  response.status(200).json(customers);
+});
+
+app.listen(3000);
